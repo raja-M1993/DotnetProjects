f9b92cab58683e8d7b2c98860ed8231d0f82d1ab
Raja <raja.m@mstsolutions.com>
2017-04-26 15:29:21 +0530

    my first commit

---
+++ concatenate data/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace concatenate_data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String val1 = textBox1.Text;
+            String val2 = textBox2.Text;
+            String myval = val1 + " " + val2;
+            label1.Text = myval;
+
+        }
+    }
+}
